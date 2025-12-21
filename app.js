14e7deada814b2906cb7f6a610cc600a8c088c7f
Shahbaz Ali <shahbasu01@gmail.com>
2025-12-21 15:14:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+// ans 1 
+// let city = "karachi";
+// ans 2
+// let city = "karachi";
+// alert(city);
+// ans 3
+// let firstName = "shahbaz";
+// let lastName = "ali";
+// ans 4 
+// let firstName = "shahbaz ";
+// let lastName = "ali";
+// let fullName = firstName + lastName;
+// alert(fullName);
+// ans 5 
+// let num = 10;
+// console.log(num)
+// ans 6 
+// let num = 2000;
+// alert(num);
+// ans 7 
+// let uname;
+// alert(uname);
+// ans 8 
+// let myAge = 20;
+// alert("my age is " + myAge )
+// ans 9 
+// let message = "hi there how are you "
+// alert(message)
+// ans 10 
+// let fmessage = "hi there ";
+// let smessage = "how are you"
+// alert(fmessage);
+// alert(smessage);
+// ans 11 
+// let question = prompt("what is your name ")
+// ans 12
+// let question = prompt("what is your name ")
+// alert("Hello, " + question);
+// ans 13
+// let age = prompt("enter your age");
+// ans 14 
+// let grong = +prompt("enter your grong");
+// ans 15 
+// let fnum = +prompt("enter a number ");
+// let snum = +prompt("again enter a number");
+// ans 16 
+// let fnum = +prompt("enter a number ");
+// let snum = +prompt("again enter a number");
+// alert(fnum + snum);
+// ans 17 
+// let city = prompt("enter your city name");
+// alert("you're living in " + city);
+// ans 18
+// let num = prompt("enter a number and see what happen");
+// let result = num * 2;
+// alert("see the magic " + result);
+// // ans 19
+// let colors = prompt("what is your favorite color");
+// alert(colors + " is your favorite color wow what a good color");
+// ans 20
+// let subtract = prompt("enter a number");
+// let result = subtract - 5;
+// alert("from your number i subtracted 5 okay there you go the number is " + result);
+// ans 21 
+// let greet = "Hello";
+// let planet = "world";
+// let sum = greet + " " + planet;
+// console.log(sum);
+// ans 22
+// let come = "home";
+// alert("wellcome " + come);
+// ans 23
+// let fullName = "shahbaz ali ";
+// let age = 20;
+// alert("my name is " + fullName + "and my age is " + age);
+// ans 24 
+// let greet = "hello";
+// let name = "ali";
+// let message =`${greet} ${name}`
+// alert(message)
+// ans 25 
+// let world = "planet"
+// let qty = 10;
+// let combine = world + qty;
+// alert(combine);
+// ans 26
+// let num1 = "5";
+// let num2 = 5;
+// let sum = num1 + num2;
+// console.log(sum)
+// ans 27 
+// let num2 = 5;
+// let num1 = "5";
+// let sum = num1 + num2;
+// console.log(sum)
+// ans 28
+let num1 = "5";
+let num2 = 5;
+let num3 = 5;
+let result = num1 + num2 + num3;
+console.log(result);
