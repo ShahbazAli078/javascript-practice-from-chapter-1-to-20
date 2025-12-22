80d4bfc736d3662d7af35edfa6cb9100dc8c6ac6
Shahbaz Ali <shahbasu01@gmail.com>
2025-12-22 12:46:15 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -94,8 +94,195 @@
 // let sum = num1 + num2;
 // console.log(sum)
 // ans 28
-let num1 = "5";
-let num2 = 5;
-let num3 = 5;
-let result = num1 + num2 + num3;
-console.log(result);
+// let num1 = "5";
+// let num2 = 5;
+// let num3 = 5;
+// let result = num1 + num2 + num3;
+// console.log(result);
+// ans 29 
+// let num2 = 5;
+// let num3 = 5;
+// let num1 = "5";
+// let result = num3 + num2 + num1;
+// console.log(result);
+// ans 30
+// let name = prompt("enter your name");
+// let age = prompt("what is your age");
+// let sum = name + age;
+// alert("my name is " + name + " and i am " + age + " years old");
+// ans 30 
+// let name = "ali";
+// let age = 20;
+// alert(`My name is ${name} and i am ${age} years old`);
+// ans 31 
+// let number = prompt("enter a number");
+// if (number > 10) {
+//     alert("the number is grester than 10");
+// }
+// ans 32
+// let num = +prompt("Enter a number");
+// if (num === 100) {
+//     alert("the number is equal to 100");
+// }
+// ans 33 
+// let age = +prompt("enter your age");
+// if (age === 18) {
+//     alert("age is equal");
+// }
+// ans 34 
+// let num = +prompt("enter a number");
+// if (num < 0) {
+//     alert("yes it is less than zero");
+// }
+// ans 35
+// let password = prompt("enter your password");
+// if (password === "admin"){
+//     alert("access granted");
+// }
+// ans 36 
+// let firstNum = prompt("ernter a Number");
+// let secondNum = prompt("again enter a number");
+// if (firstNum === secondNum) {
+//     alert("these numbers are equal")
+// }
+// ans 37
+// let qty = prompt("how much do you have with you");
+// let profit = prompt("how much do you profit from this");
+// if (qty !== profit) {
+//     alert("you in profit");
+// }
+// ans 38
+// let num = +prompt("enter a number");
+// if (num % 2 === 0) {
+//     alert("even number");
+// }
+// ans 39
+// let num = +prompt("enter a number");
+// if (num % 2 === 1) {
+//     alert("odd number");
+// }
+// ans 40 
+// let guess = prompt("Type yes or no");
+// if (guess === "yes") {
+//     alert("correct");
+// }
+// else{
+//     alert("you did not type 'yes'");
+// }
+// ans 41
+// let temprature = prompt("what is your room temprature");
+// if (temprature > 30) {
+//     alert("temprature is greater than thirty");
+// }
+// else{
+//     alert("temprature is less than thirty");
+// }
+// ans 42 
+// let num1 = prompt("enter a number");
+// let num2 = prompt("again enter a number");
+// if (num1 > num2) {
+//     alert("num1 is greater");
+// }
+// else if (num1 < num2) {
+//     alert("num2 is greater");
+// }
+// else {
+//     alert("they are equal");
+// }
+// ans 43
+// let num = +prompt("enter a number");
+// if (num > 0) {
+//     alert("number is positive");
+// }
+// ans 44
+// let num = +prompt("enter a number");
+// if (num === 0) {
+//     alert("number is zero");
+// }
+// ans 45
+// let num = +prompt("enter a number");
+// if (num % 5 === 0) {
+//     alert("yeah this number is divisible by 5")
+// }
+// ans 46
+// let num = prompt("enter a number");
+// if (num % 2 === 0){
+//     alert("this num is oven num");
+// }
+// else{
+//     alert("this num is odd num");
+// }
+// ans 47
+// let num = prompt("enter a number");
+// if (num % 2 === 0) {
+//     alert("this is even number")
+// }
+// else if (num % 2 !== 0) {
+//     alert("this is odd number")
+// }
+// ans 48
+// let age = prompt("enter your age");
+// if (age > 20) { 
+//     alert("you are elligibale for vote");
+// }
+// ans 49
+// let number = +prompt("enter a number");
+// if (number === 0 || number < 0) {
+//     alert("yes this is nagative number")
+// } 
+// ans 50
+// let marks = +prompt("enter your  marks number");
+// if (marks >= 75) {
+//     alert("you are passed");
+// } else{
+//     alert("you are falid");
+// }
+// ans 51
+// let userName = prompt("enter user name");
+// let password = prompt("enter password number");
+// if (userName === "admin" && password === "1234") {
+//     alert("login sucssessfull");
+// } else{
+//     alert("invalid username or password");
+// }
+// ans 52
+// let num = +prompt("enter a number",9);
+// if (num < "10") {
+//     alert("your number is less than 10")
+// } else if (num > "10") {
+//     alert("your number is greater than 10");
+// } else{
+//     alert("your number is 10")
+// }
+// ans 53
+// let marks = +prompt("enter your marks number");
+// if (marks >= "80") {
+//     alert("Grade A");
+// } else if (marks >= "70") {
+//     alert("Grade B");
+// } else if (marks >= "60") {
+//     alert("grade C") 
+// } else if (marks >= 50) {
+//     alert("Grade D")
+// } else{
+//     alert("failed")
+// }
+// ans 54
+// let temp = +prompt("enter your room temprature");
+// if (temp < "5") {
+//     alert("your room teprature is cold");
+// } else if (temp <= "25") {
+//     alert("your room temprature is normal");
+// } else{
+//     alert("your temperature is hot");
+// }
+// ans 55
+// let num = +prompt("enter a number");
+// if (num < "0") {
+//     alert("nagetive number");
+// } else if (num > "0"){
+//     alert("possitive number");
+// }
+// else{
+//     alert("the number is zero")
+// }
