ed8a79209e50e9d4d127557da06f307c3e6b84a8
Shahbaz Ali <shahbasu01@gmail.com>
2025-12-24 14:35:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -286,3 +286,266 @@
 // else{
 //     alert("the number is zero")
 // }
+// ans 55
+// let password = prompt("ener a password");
+// if (password === "1234") {
+//     alert("acsess granted");
+// } else{
+//     alert("password invalid");
+// }
+// ans 56
+// let compare1 = +prompt("enter first number");
+// let compare2 = +prompt("enter second number");
+// if (compare1 > compare2) {
+//     alert(compare1 + " is bigger");
+// } else if (compare1 < compare2) {
+//     alert(compare2 + " is bigger")
+// } else{
+//     alert("both numbers are equal")
+// }
+// ans 57
+// let age = +prompt("enter your age");
+// if (age < "13") {
+//     alert("you are kid");
+// }
+// else if (age < "20") {
+//     alert("you are a teen");
+// }
+// else {
+//     alert("you are an adult");
+// }
+// ans 58
+// let traffic = prompt("enter the traffic light color");
+// if (traffic === "red") {
+//     alert("stop the traffic color is red");
+// } else if(traffic === "amber") {
+//     alert("wait the traffic color is amber");
+// } else {
+//     alert("go the traffic color is green");
+// }
+// ans 59
+// let date = prompt("enter your day");
+// if (date === "1") {
+//     alert("your day is monday 1");
+// }
+// else if (date === "2") {
+//     alert("your day is tuesday ");
+// }
+// else if (date === "3") {
+//     alert("your day is wednesday ");
+// }
+// else if (date === "4") {
+//     alert("your day is thursday ");
+// }
+// else if (date === "5") {
+//     alert("your day is firday ");
+// }
+// else if (date === "6") {
+//     alert("your day is sturday ");
+// }
+// else if (date === "7") {
+//     alert("your day is sunday ");
+// } else {
+//     alert("invalid number form 1 to 7");
+// }
+// ans 60
+// let time = +prompt ("enter your time");
+// if (time < 12 && time >= 6) {
+//     alert("your timing is right now morning");
+// } else if (time >= 12 && time < 18) {
+//     alert("your timing is now afternoon");
+// } else if (time >= 18 && time <= 23) {
+//     alert("your timing is now night");
+// } else {
+//     alert("invalid number");
+// }
+// ans 61
+// let number = +prompt("enter a number to check is the number is divisible by 3")
+// if (number % 3 === 0) {
+//     alert("the number is divisible by number 3");
+// } else{
+//     alert("the number is not divisible by number 3");
+// }
+// ans 62
+// let num = +prompt("enter a number to check if the number is divisibl by 3 and 5.");
+// if (num % 2 === 0) {
+//     alert("the number is divisible by 2");
+// } else if (num % 5 === 0) {
+//     alert("the number is divisible by 5");
+// } else {
+//     alert("the number is not divisible by 3 or 5");
+// } 
+// ans 63
+// let num = +prompt("enter a number to check if the number is multple of 10");
+// if (num % 10 === 0) {
+//     alert(" the number is a multiple of ten")
+// } else {
+//     alert("the number is not a multiple of ten")
+// }
+// ans 64
+// let age = prompt("enter your age");
+// let purchaseAmount = prompt("enter your purches amount in $");
+// if (age >= 60 || purchaseAmount === 100) {
+//     alert("you are eligable for discount");
+// } else {
+//     alert("you are not eligable for discount")
+// }
+// ans 65
+// let grade = +prompt("enter your grade in exam");
+// if (grade >= 95) {
+//     alert("A++")
+// } else if (grade >= 85) {
+//     alert("B")
+// } else if (grade >= 75) {
+//     alert("C")
+// } else if (grade >= 50) {
+//     alert("E")
+// } else{
+//     alert("fail")    
+// } 
+// the arrays start from here
+// ans 66
+// let colors = ["red", "yellow", "blue", "green", "white"]
+// ans 67 
+// let colors = ["red", "yellow", "blue", "green", "white"]
+// alert(colors[0])
+// ans 68
+// let colors = ["red", "yellow", "blue", "green", "white"]
+// alert(colors[colors.length - 1])
+// ans 69
+// let unames = ["ali", "khan", "zia", "asif"]
+// let uname = unames.splice(2, 2);
+// console.log(uname)
+// ans 70
+// let pets = ["dog", "cat", "hamster"];
+// console.log(pets.length)
+// ans 71
+// let city = ["karachi", "lahore", "pindiy"];
+// city.push("panjab")
+// console.log(city) 
+// ans 72
+// let city = ["karachi", "lahore", "pindiy"];
+// city.pop();
+// console.log(city);
+// ans 73
+// let city = ["karachi", "lahore", "pindiy"];
+// city.unshift("name");
+// console.log(city);
+// ans 74
+// let city = ["karachi", "lahore", "pindiy"];
+// city.shift();
+// console.log(city)
+// ans 75
+// let number = [1, 2, 3, 4, 5];
+// ans 76
+// let number = [1, 2, 3, 4, 5];
+// let newNumber = number[2];
+// console.log(newNumber)
+// ans 77
+// let number = [1, 2, 3, 4, 5];
+// number[2] = 5;
+// console.log(number);
+// ans 78
+// let colors = [];
+// colors[0] = "purple"
+// colors[1] = "blue"
+// colors[2] = "red"
+// colors[3] = "green"
+// colors[4] = "yellow"
+// console.log(colors);
+// ans 79
+// let array = ["ali", "asif", "khan", "asad"];
+// array[2] = "shahbaz"
+// alert("array length is : " + array.length)
+// array.push("black");
+// alert("New length is:" + array.length)
+// ans 80
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"];
+// ans 81
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"];
+// mixedArray.push("horse", 7);
+// console.log(mixedArray);
+// ans 82
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"]
+// mixedArray.pop();
+// console.log(mixedArray);
+// ans 83
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"]
+// mixedArray.unshift("nurse", "patient");
+// console.log(mixedArray)
+// ans 84
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"]
+// mixedArray.shift();
+// console.log(mixedArray);
+// ans 85
+// let mixedArray = ["dogs", 4, "cats", 7, "birds"]
+// mixedArray.shift();
+// alert(mixedArray);
+// ans 86
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.push("pakistan");
+// console.log(country);
+// ans 87
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.pop();
+// console.log(country);
+// ans 88
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.shift();
+// console.log(country);
+// ans 89
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.unshift("pakistan");
+// console.log(country);
+// ans 90
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.splice(1, 3);
+// console.log(country);
+// ans 91
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.splice(2, 1, "pakistan", "arabs");
+// console.log(country);
+// ans 92
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.splice(2, 1);
+// console.log(country);
+// ans 93
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.splice(2, 1, "arabs");
+// console.log(country);
+// ans 94
+// var country = ["india", "europe", "italy", "china", "france"];
+// country.slice(1, 5);
+// console.log(newCountry);
+// ans 95
+// var country = ["india", "europe", "italy", "china", "france"];
+// let newCountry = country.slice(1, 5);
+// console.log(newCountry);
+// ans 96
+// var country = ["india", "europe", "italy", "china", "france"];
+// let newCountry = country.slice(0, 2)
+// console.log(newCountry)
+// country.splice(2, 1, "pakistan")
+// console.log(country);
+// ans 97
+// let friuts = ["mango", "banana", "orange", "apple"]
+// console.log(friuts)
+// friuts.splice(2, 1);
+// console.log(friuts)
+// ans 98
+// let arr = [1, 2, 3];
+// arr.push(4);
+// arr.pop();
+// console.log(arr); answer is 1,2,3
+// ans 99
+// let arr = ['a', 'b'];
+// arr.unshift('x');
+// arr.shift();
+// console.log(arr); answer is a b
+// ans 100
+// let fruits = ["apple", "banana", "mango"];
+// fruits.push("orange");
+// fruits.unshift("kiwi");
+// fruits.splice(2, 1, "grape"); answer is kiwi Apple grape mango orange 
+
+
